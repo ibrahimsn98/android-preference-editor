56cb3a9c7886ed1219f987cda19abdcf399f5007
melianor <ibrahimsn98@gmail.com>
2019-02-12 18:52:56 +0300

    add readme

---
+++ main/readme.md
@@ -0,0 +1,8 @@
+## Android Shared Preferences Editor
+Change the preferences of the applications.
+
+### Usage
+1) Start the emulator or connect your device via USB.<br>
+`emulator -avd Pixel_XL_API_27`
+2) Run the python script<br>
+`python3 run.py`
--- main/run.py
+++ main/run.py
@@ -1,4 +1,5 @@
-import json
+import os
+import re
 import subprocess
 import sys
 import time
@@ -15,7 +16,7 @@ def intInput(desc):
     return data
 
 
-print('Initializing ADB as a root...')
+print('Initializing ADB as root...')
 
 # Start adb as root
 subprocess.call("adb root", shell=True)
@@ -27,22 +28,27 @@ packages = subprocess.check_output("adb shell 'pm list packages -f' | sed -e 's/
 # Remove empty package
 packages.pop()
 
+t = len(packages)
+c = 0
+
 # Iterate packages
-for i, package in enumerate(packages):
+for i, package in enumerate(list(packages)):
 
     # Show progressbar
     sys.stdout.write('\r')
-    sys.stdout.write("Getting app packages [%-30s] %d%%" % ('=' * int((i+1) / len(packages) * 30), int((i+1) / len(packages) * 100)))
+    sys.stdout.write("Getting " + str(t) + " packages [%-30s] %d%%" %
+                     ('=' * int((i + 1) / t * 30), int((i + 1) / t * 100)))
     sys.stdout.flush()
 
     # Remove packages that doesn't have any preference files
     check_pref_file_str = "adb shell 'ls /data/data/" + package + "/shared_prefs'"
-    check_pref_file = subprocess.check_output(check_pref_file_str, shell=True).decode('UTF-8')
-    if 'No such file or directory' in check_pref_file:
-        packages.remove(package)
+    check_pref_file = subprocess.check_output(check_pref_file_str, shell=True).decode('UTF-8').strip()
 
-# New line
-print("")
+    if 'No such file' in check_pref_file or check_pref_file == "":
+        packages.remove(package)
+        c += 1
+
+print("\n" + str(c) + " apps which hasn't any preference files are ignored\n")
 
 for i, package in enumerate(packages):
     print(str(i) + ') ' + package)
@@ -52,15 +58,15 @@ package_index = intInput('\nSelect app package: ')
 if package_index < 0 or package_index > len(packages) - 1:
     sys.exit(0)
 
-package = packages[package_index]
-print(package)
+package = packages[package_index].strip()
+print(package + '\n')
 
 # Get preference files of selected package
 pref_files_str = "adb shell 'ls /data/data/" + package + "/shared_prefs'"
-pref_files = subprocess.check_output(pref_files_str, shell=True).decode('UTF-8').split('\n')
+pref_files = subprocess.check_output(pref_files_str, shell=True).decode('UTF-8')
 
-# Remove empty file name
-pref_files.pop()
+# Find xml file names
+pref_files = re.findall(r'[\w.-]+\.xml', pref_files)
 
 while True:
     for i, pref_file in enumerate(pref_files):
@@ -77,6 +83,7 @@ while True:
         continue
 
     pref_file = pref_files[pref_file_index]
+    print(pref_file + '\n')
 
     # Get preference file content
     pref_file_data_str = "adb shell 'cat /data/data/" + package + "/shared_prefs/" + pref_file + "'"
@@ -89,10 +96,10 @@ while True:
         for i, child in enumerate(root):
             if child.tag == 'string':
                 print(str(i) + ') ', child.tag, child.attrib['name'], child.text)
-            else:
+            elif child.tag == 'int' or child.tag == 'long' or child.tag == 'boolean':
                 print(str(i) + ') ', child.tag, child.attrib['name'], child.attrib['value'])
 
-        print('-1) Back to previous page')
+        print('-1) Back to select preference file')
         pref_index = intInput('\nSelect preference to edit: ')
 
         if pref_index == -1:
@@ -104,18 +111,39 @@ while True:
 
         pref = root[pref_index]
 
-        pref_new_value = input('\nEnter new value: ')
+        while True:
+            pref_new_value = input('Enter new value: ')
+
+            # Boolean value check
+            if pref.tag == 'boolean' and (pref_new_value != 'true' and pref_new_value != 'false'):
+                print('You can only write true or false!\n')
+                continue
+
+            # Numeric value check
+            if pref.tag == 'int' or pref.tag == 'long':
+                try:
+                    int(pref_new_value)
+                except:
+                    print('You can only write numeric!\n')
+                    continue
+
+            break
 
         if pref.tag == 'string':
             root[pref_index].text = pref_new_value
-        else:
+        elif pref.tag == 'int' or pref.tag == 'long' or pref.tag == 'boolean':
             root[pref_index].attrib['value'] = pref_new_value
 
         # Update preference file content with decoded xmlTree
-        pref_update_str_content = '<?xml version="1.0" encoding="utf-8" standalone="yes" ?>\n' + xmlTree.tostring(root) \
-            .decode('UTF-8')
+        pref_update_str_content = '<?xml version="1.0" encoding="utf-8" standalone="yes" ?>\n' \
+                                  + xmlTree.tostring(root).decode('UTF-8')
 
-        pref_update_str = "adb shell echo '" + json.dumps(pref_update_str_content) + " > /data/data/" \
-                          + package + "/shared_prefs/" + pref_file + "'"
+        # Write new value to preference file
+        f = open(pref_file, "w")
+        f.write(pref_update_str_content)
+        f.close()
+
+        # Update preference file
+        pref_update_str = 'adb push ' + os.getcwd() + '/' + pref_file + ' /data/data/' + package + '/shared_prefs'
         pref_update = subprocess.check_output(pref_update_str, shell=True).decode('UTF-8')
-        print('Preference updated!')
+        print('Preference value updated!\n')
